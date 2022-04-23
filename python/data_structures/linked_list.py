562b12529816e924f9e426f71ea4f5342ebf2de8
James Brooks <brooksjamesk01@gmail.com>
2022-04-22 22:46:39 -0700

    finished all code to pass all tests

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -4,12 +4,41 @@ class LinkedList:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
+    def __str__(self):
+        return_string = self.concat_list()
+        if self.head == None:
+            return "NULL"
+        return f"{return_string}NULL"
+
+    def insert(self, value):
         # method body here
-        pass
+        newNode = Node()
+        newNode.value = value
+        newNode.ref = self.head
+        self.head = newNode
+
+    def concat_list(self):
+        list_item = self.head
+        linked_list_string = ""
+        while list_item is not None:
+            linked_list_string += f"{{ {list_item.value} }} -> "
+            list_item = list_item.ref
+        return linked_list_string
+
+    def includes(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.ref
+        return False
+
+class Node:
+    def __init__(data_node, data = None):
+        data_node.value = data
+        data_node.ref = "NULL"
 
 
 class TargetError:
