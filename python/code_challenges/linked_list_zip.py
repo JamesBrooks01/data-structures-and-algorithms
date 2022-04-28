2c27f93716f1f3cf4a86582d588902535a25dc11
James Brooks <brooksjamesk01@gmail.com>
2022-04-27 20:49:48 -0700

    made variable names more python correct

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,34 +1,34 @@
 from data_structures.linked_list import LinkedList
 
 def zip_lists(a, b):
-    newList = LinkedList()
-    current1 = a.head
-    current2 = b.head
+    new_list = LinkedList()
+    current_1 = a.head
+    current_2 = b.head
     if a.head == None:
         return b
     if b.head == None:
         return a
-    newList.insert(current1.value)
+    new_list.insert(current_1.value)
 
-    while newList:
-        print(current1.next, current2.next)
-        if current1.next == None and current2.next == None:
-            newList.append(current2.value)
+    while new_list:
+        print(current_1.next, current_2.next)
+        if current_1.next == None and current_2.next == None:
+            new_list.append(current_2.value)
             break
-        if current1.next == None and current2.next != None:
-            newList.append(current2.value)
-            next_value = current2.next
-            newList.append(next_value.value)
+        if current_1.next == None and current_2.next != None:
+            new_list.append(current_2.value)
+            next_value = current_2.next
+            new_list.append(next_value.value)
             break
-        if current1.next != None and current2.next == None:
-            newList.append(current2.value)
-            newList.append(current1.next.value)
+        if current_1.next != None and current_2.next == None:
+            new_list.append(current_2.value)
+            new_list.append(current_1.next.value)
             break
-        newList.append(current2.value)
-        newList.append(current1.next.value)
-        current1 = current1.next
-        current2 = current2.next
-    print(newList)
+        new_list.append(current_2.value)
+        new_list.append(current_1.next.value)
+        current_1 = current_1.next
+        current_2 = current_2.next
+    print(new_list)
 
-    return newList
+    return new_list
 
