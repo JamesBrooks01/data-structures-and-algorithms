108d4cde7bd6821756e378381c7867c4f0d94f7a
James Brooks <brooksjamesk01@gmail.com>
2022-04-29 21:10:29 -0700

    completed queue.py

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -2,36 +2,32 @@ from data_structures.linked_list import Node
 from data_structures.invalid_operation_error import InvalidOperationError
 
 class Queue:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        self.front_of_queue = None
-        self.back_of_queue = None
+        self.front = None
+        self.back = None
 
     def enqueue(self, value):
         new_addition = Node(value)
-        if self.back_of_queue:
-            self.back_of_queue.next = new_addition
-        self.back_of_queue = new_addition
-        if not self.front_of_queue:
-            self.front_of_queue = self.back_of_queue
+        if self.back:
+            self.back.next = new_addition
+        self.back = new_addition
+        if not self.front:
+            self.front = self.back
 
     def dequeue(self):
-        if not self.front_of_queue:
+        if not self.front:
             raise InvalidOperationError("Queue Empty, Try Again Later")
-        old_front_of_queue = self.front_of_queue
-        self.front_of_queue = old_front_of_queue.next
-        return old_front_of_queue.value
+        old_front = self.front
+        self.front = old_front.next
+        return old_front.value
 
     def peek(self):
-        if not self.front_of_queue:
+        if not self.front:
             raise InvalidOperationError("Queue Empty, Try Again Later")
-        return self.front_of_queue.value
+        return self.front.value
 
-    def IsEmpty(self):
-        if self.front_of_queue == None:
+    def is_empty(self):
+        if self.front == None:
             return True
         else:
             return False
