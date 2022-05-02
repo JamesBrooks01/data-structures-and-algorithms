1616c364a016e602a5a8daeafe43f629f3024604
James Brooks <brooksjamesk01@gmail.com>
2022-05-02 13:50:01 -0700

    added a few tests to verify the function logic worked properly

--- python/tests/code_challenges/test_stack_queue_pseudo.py
+++ python/tests/code_challenges/test_stack_queue_pseudo.py
@@ -46,3 +46,101 @@ def test_enqueue_dequeue_enqueue_dequeue():
     expected = ["bananas", "cucumbers", "dates"]
 
     assert actual == expected
+
+def test_pseudo_queue_was_it_that_easy():
+    pq = PseudoQueue()
+    pq.enqueue("1")
+    pq.enqueue("2")
+    pq.enqueue("3")
+    result_1 = pq.dequeue()
+    pq.dequeue()
+    pq.enqueue("4")
+    pq.enqueue("5")
+    pq.enqueue("6")
+    pq.dequeue()
+    result_2 = pq.dequeue()
+
+    expected_1 = "1"
+    expected_2 = "4"
+
+    assert result_1 == expected_1
+    assert result_2 == expected_2
+
+def test_pseudo_queue_was_it_that_easy_2():
+    pq = PseudoQueue()
+    pq.enqueue("1")
+    pq.enqueue("2")
+    pq.enqueue("3")
+    result_1 = pq.dequeue()
+    pq.enqueue("4")
+    pq.enqueue("5")
+    pq.enqueue("6")
+    result_2 = pq.dequeue()
+
+    expected_1 = "1"
+    expected_2 = "2"
+
+    assert result_1 == expected_1
+    assert result_2 == expected_2
+
+def test_pseudo_queue_was_it_that_easy_3():
+    pq = PseudoQueue()
+    pq.enqueue("1")
+    pq.enqueue("2")
+    pq.enqueue("3")
+    result_1 = pq.dequeue()
+    pq.dequeue()
+    pq.dequeue()
+    pq.enqueue("4")
+    pq.enqueue("5")
+    pq.enqueue("6")
+    pq.dequeue()
+    result_2 = pq.dequeue()
+
+    expected_1 = "1"
+    expected_2 = "5"
+
+    assert result_1 == expected_1
+    assert result_2 == expected_2
+
+def test_pseudo_queue_was_it_that_easy_4():
+    pq = PseudoQueue()
+    pq.enqueue("1")
+    pq.enqueue("2")
+    pq.enqueue("3")
+    result_1 = pq.dequeue()
+    pq.dequeue()
+    pq.dequeue()
+    pq.enqueue("4")
+    pq.enqueue("5")
+    pq.enqueue("6")
+    pq.dequeue()
+    pq.dequeue()
+    result_2 = pq.dequeue()
+
+    expected_1 = "1"
+    expected_2 = "6"
+
+    assert result_1 == expected_1
+    assert result_2 == expected_2
+
+def test_pseudo_queue_was_it_that_easy_5():
+    pq = PseudoQueue()
+    pq.enqueue("1")
+    pq.enqueue("2")
+    pq.enqueue("3")
+    pq.dequeue()
+    result_1 = pq.dequeue()
+    pq.dequeue()
+    pq.enqueue("4")
+    pq.enqueue("5")
+    pq.enqueue("6")
+    pq.dequeue()
+    pq.dequeue()
+    result_2 = pq.dequeue()
+
+    expected_1 = "2"
+    expected_2 = "6"
+
+    assert result_1 == expected_1
+    assert result_2 == expected_2
