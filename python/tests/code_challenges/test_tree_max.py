34f90492f616b656a748615f1cc4364db9c42811
James Brooks <brooksjamesk01@gmail.com>
2022-05-09 15:02:41 -0700

    commented out test and added two of my own

--- python/tests/code_challenges/test_tree_max.py
+++ python/tests/code_challenges/test_tree_max.py
@@ -2,7 +2,7 @@ import pytest
 from data_structures.binary_tree import BinaryTree, Node
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_max_val():
     tree = BinaryTree()
     tree.root = Node(10)
@@ -12,4 +12,34 @@ def test_max_val():
     actual = tree.find_maximum_value()
     expected = 30
 
+    assert actual == expected
+
+def test_max_val_larger_tree():
+    tree = BinaryTree()
+    tree.root = Node(1)
+    tree.root.left = Node(51)
+    tree.root.right = Node(364)
+    tree.root.left.left = Node(45637)
+    tree.root.left.right = Node(2134)
+    tree.root.right.left = Node(753)
+    tree.root.right.right = Node(64)
+
+    actual = tree.find_maximum_value()
+    expected = 45637
+    assert actual == expected
+
+def test_max_val_example():
+    tree = BinaryTree()
+    tree.root = Node(2)
+    tree.root.left = Node(7)
+    tree.root.right = Node(5)
+    tree.root.left.left = Node(2)
+    tree.root.right.right = Node(9)
+    tree.root.left.right = Node(6)
+    tree.root.right.right.left = Node(4)
+    tree.root.left.right.left = Node(5)
+    tree.root.left.right.right = Node(11)
+
+    actual = tree.find_maximum_value()
+    expected = 11
     assert actual == expected
