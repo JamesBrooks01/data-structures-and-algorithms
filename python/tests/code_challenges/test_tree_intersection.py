733304dc0f006f807dfc267a6b5874b05612fedb
James Brooks <brooksjamesk01@gmail.com>
2022-05-31 22:11:11 -0700

    adapted the existing tests and added a few new tests for challenge 32

--- python/tests/code_challenges/test_tree_intersection.py
+++ python/tests/code_challenges/test_tree_intersection.py
@@ -7,8 +7,7 @@ from data_structures.queue import Queue
 def test_exists():
     assert tree_intersection
 
-
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_tree_intersection():
 
     tree_a = BinaryTree()
@@ -24,7 +23,47 @@ def test_tree_intersection():
 
     assert sorted(actual) == sorted(expected)
 
+def test_additional_value():
+    tree_a = BinaryTree()
+    values = [1,2,3,4,5,6]
+    add_values_to_empty_tree(tree_a, values)
+    tree_b = BinaryTree()
+    values = [7,8,9,10,5,3,2]
+    add_values_to_empty_tree(tree_b, values)
 
+    actual = tree_intersection(tree_a,tree_b)
+    expected = [5,3,2]
+
+    assert sorted(actual) == sorted(expected)
+
+def test_non_numerical_values():
+    tree_a = BinaryTree()
+    values = ['abab','cdcd','efef','ghgh','ijoj','klkl','mnmn','opop',]
+    add_values_to_empty_tree(tree_a, values)
+    tree_b = BinaryTree()
+    values = ['qrqr','stst','uvuv', 'wxwx','yzyz', 'abab','efef']
+    add_values_to_empty_tree(tree_b, values)
+
+    actual = tree_intersection(tree_a,tree_b)
+    expected = ['abab','efef']
+
+    assert sorted(actual) == sorted(expected)
+
+
+def test_booleans():
+    tree_a = BinaryTree()
+    values = [True]
+    add_values_to_empty_tree(tree_a, values)
+    tree_b = BinaryTree()
+    values = [True, False]
+    add_values_to_empty_tree(tree_b, values)
+
+    actual = tree_intersection(tree_a, tree_b)
+    expected = [True]
+
+    assert sorted(actual) == sorted(expected)
+
+# @pytest.mark.skip("TODO")
 def add_values_to_empty_tree(tree, values):
     """
     Helper function to add given values to BinaryTree
