542012aa14b497a23ecf24545830a8ac03e0cf5b
James Brooks <brooksjamesk01@gmail.com>
2022-06-01 17:57:05 -0700

    finished code for python challenge 33 Hashtable_Left_Join

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,14 @@
-from data_structures.hashtable import Hashtable
-
-
-def left_join():
-    pass
+def left_join(left, right):
+    return_list = []
+    for item in left:
+        item_list = []
+        item_list.append(item)
+        value = left[item]
+        item_list.append(value)
+        if item in right:
+            value2 = right[item]
+            item_list.append(value2)
+        else:
+            item_list.append("NONE")
+        return_list.append(item_list)
+    return return_list
